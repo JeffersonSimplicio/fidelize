5ca5f552a2d2d8546ffa2e3731916c2e3ab0a4f2
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-09-23 10:15:05 -0300

    feat: implement customer management with mock database and display list

--- app/(tabs)/customers.tsx
+++ app/(tabs)/customers.tsx
@@ -1,17 +1,34 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import { Customer } from "../../interfaces/customers";
+import { customersDb } from "../../database/customersDb";
 
 export default function Customers() {
+  const [customers, setCustomers] = useState<Customer[]>([]);
+
+  useEffect(() => {
+    const fetchCustomers = async () => {
+      const customers = await customersDb.getAll();
+      setCustomers(customers);
+    };
+
+    fetchCustomers();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Clientes</Text>
+    <View style={{ flex: 1, padding: 16 }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>
+        Lista de Clientes
+      </Text>
+      <FlatList
+        data={customers}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Text>
+            {item.name} - {item.points} pontos
+          </Text>
+        )}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
---
+++ interfaces/customers.ts
@@ -0,0 +1,7 @@
+export type Customer = {
+  id: number;
+  name: string;
+  phone: string;
+  points: number;
+  lastVisitAt: string;
+};
