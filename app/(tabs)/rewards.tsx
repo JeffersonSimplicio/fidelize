ea875ece37e87d9d424e118e4254846bd9556acb
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-09-23 18:03:16 -0300

    feat: add refresh control to rewards list for better user experience

--- app/(tabs)/rewards.tsx
+++ app/(tabs)/rewards.tsx
@@ -1,41 +1,51 @@
-import { FlatList, Text, View } from 'react-native';
-import { Reward } from '../../interfaces/reward';
-import { rewardsDb } from '../../database/rewardsDb';
-import { useEffect, useState } from 'react';
-import { Link } from 'expo-router';
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { FlatList, RefreshControl, Text, View } from "react-native";
+import { Reward } from "../../interfaces/reward";
+import { rewardsDb } from "../../database/rewardsDb";
+import { useEffect, useState } from "react";
+import { Link } from "expo-router";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
 export default function Rewards() {
   const [rewards, setRewards] = useState<Reward[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const fetchRewards = async () => {
+    const rewards = await rewardsDb.getAll();
+    setRewards(rewards);
+  };
 
   useEffect(() => {
-    const fetchRewards = async () => {
-      const rewards = await rewardsDb.getAll();
-      setRewards(rewards);
-    };
-
     fetchRewards();
   }, []);
 
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await fetchRewards();
+    setRefreshing(false);
+  };
+
   return (
-      <View style={{ flex: 1, padding: 16 }}>
-        <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>
-          Lista de Recompensas
-        </Text>
-        <FlatList
-          data={rewards}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <Link href={`/details/reward/${item.id}`}>
-              <Text>
-                {item.name} - {item.pointsRequired} pontos necessários
-              </Text>
-            </Link>
-          )}
-        />
-        <Link href="/create/reward">
-          <FontAwesome6 name="add" size={24} color="black" />
-        </Link>
-      </View>
-    );
+    <View style={{ flex: 1, padding: 16 }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>
+        Lista de Recompensas
+      </Text>
+      <FlatList
+        data={rewards}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Link href={`/details/reward/${item.id}`}>
+            <Text>
+              {item.name} - {item.pointsRequired} pontos necessários
+            </Text>
+          </Link>
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+      <Link href="/create/reward">
+        <FontAwesome6 name="add" size={24} color="black" />
+      </Link>
+    </View>
+  );
 }
