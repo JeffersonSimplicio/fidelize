f9d47ac139e2df10aa0ac111cc02b2a0b9b3e857
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-09-24 16:08:21 -0300

    feat: remove editing functionality from customer details screen and streamline display

---
+++ app/customers/[id]/edit.tsx
@@ -0,0 +1,123 @@
+import { AppButton } from "@/components/app-button";
+import { customersDb } from "@/database/customersDb";
+import { Customer } from "@/interfaces/customer";
+import { AntDesign, FontAwesome, MaterialIcons } from "@expo/vector-icons";
+import { useRouter, Stack, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  View,
+  Text,
+  Alert,
+} from "react-native";
+
+export default function CustomerEditScreen() {
+  const router = useRouter();
+  const { id } = useLocalSearchParams();
+  const [customer, setCustomer] = useState<Customer | null>(null);
+  const [name, setName] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [points, setPoints] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchCustomers = async () => {
+      const fetchedCustomer = await customersDb.getById(
+        parseInt(id as string, 10)
+      );
+      if (fetchedCustomer) {
+        setCustomer(fetchedCustomer);
+        setName(fetchedCustomer.name);
+        setPhone(fetchedCustomer.phone);
+        setPoints(fetchedCustomer.points);
+      }
+    };
+
+    fetchCustomers();
+  }, [id]);
+
+  const increasePoints = () => setPoints((prev) => prev + 1);
+  const decreasePoints = () => setPoints((prev) => (prev > 0 ? prev - 1 : 0));
+
+  const handlePointsChange = (text: string) => {
+    const numericValue = parseInt(text, 10);
+    if (isNaN(numericValue) || numericValue < 0) {
+      setPoints(0);
+    } else {
+      setPoints(numericValue);
+    }
+  };
+
+  const handleSave = async () => {
+    if (!customer) return;
+    const updatedCustomer = { ...customer, name, phone, points };
+    await customersDb.update(parseInt(id as string, 10), updatedCustomer);
+    Alert.alert("Dados atualizados com sucesso!");
+    router.back();
+  };
+
+  const handleCancelEditing = () => {
+    router.back();
+  };
+
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          title: "Editar Cliente",
+        }}
+      />
+      <View>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : undefined}
+        >
+          <View>
+            <Text>Editar Cliente</Text>
+          </View>
+          <View>
+            <Text>Nome:</Text>
+            <TextInput
+              placeholder="Digite o nome do cliente"
+              value={name}
+              onChangeText={setName}
+            />
+          </View>
+          <View>
+            <Text>Telefone:</Text>
+            <TextInput
+              placeholder="Digite o telefone"
+              keyboardType="phone-pad"
+              value={phone}
+              onChangeText={setPhone}
+            />
+          </View>
+
+          <View>
+            <AppButton onPress={decreasePoints}>
+              <AntDesign name="minus" size={24} color="black" />
+            </AppButton>
+
+            <TextInput
+              keyboardType="numeric"
+              value={points.toString()}
+              onChangeText={handlePointsChange}
+            />
+            <AppButton onPress={increasePoints}>
+              <AntDesign name="plus" size={24} color="black" />
+            </AppButton>
+          </View>
+
+          <View>
+            <AppButton onPress={handleSave}>
+              <FontAwesome name="save" size={30} color="black" />
+            </AppButton>
+            <AppButton onPress={handleCancelEditing}>
+              <MaterialIcons name="cancel" size={30} color="black" />
+            </AppButton>
+          </View>
+        </KeyboardAvoidingView>
+      </View>
+    </>
+  );
+}
--- app/customers/[id]/index.tsx
+++ app/customers/[id]/index.tsx
@@ -5,40 +5,12 @@ import { customersDb } from "@/database/customersDb";
 import { Customer } from "@/interfaces/customer";
 import DeleteButton from "@/components/delete-button";
 import { AppButton } from "@/components/app-button";
-import { GestureHandlerRootView, TextInput } from "react-native-gesture-handler";
-import { MaterialIcons, FontAwesome } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function CustomerDetailsScreen() {
   const [customer, setCustomer] = useState<Customer | null>(null);
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedName, setEditedName] = useState("");
-  const [editedPhone, setEditedPhone] = useState("");
   const route = useRouter();
   const { id } = useLocalSearchParams();
-
-  const startEditing = () => {
-    if (!customer) return;
-    setEditedName(customer.name);
-    setEditedPhone(customer.phone);
-    setIsEditing(true);
-  };
-
-  const handleCancelEditing = () => {
-    setEditedName("");
-    setEditedPhone("");
-    setIsEditing(false);
-  };
-
-  const handleSave = async () => {
-    if (!customer) return;
-    const updatedCustomer = { ...customer, name: editedName, phone: editedPhone };
-    await customersDb.update(parseInt(id as string, 10), updatedCustomer);
-    setCustomer(updatedCustomer);
-    setEditedName("");
-    setEditedPhone("");
-    setIsEditing(false);
-    Alert.alert("Dados atualizados com sucesso!");
-  };
 
   const handleDelete = () => {
     customersDb.remove(parseInt(id as string, 10));
@@ -73,7 +45,7 @@ export default function CustomerDetailsScreen() {
   }
 
   return (
-    <GestureHandlerRootView >
+    <>
       <Stack.Screen
         options={{
           title: customer.name,
@@ -81,50 +53,21 @@ export default function CustomerDetailsScreen() {
       />
       <View style={styles.container}>
         <Text>Client</Text>
-        {/* Nome */}
-        {isEditing ? (
-          <TextInput
-            value={editedName}
-            onChangeText={setEditedName}
-          />
-        ) : (
-          <Text>Name: {customer.name}</Text>
-        )}
-  
-        {/* Telefone */}
-        {isEditing ? (
-          <TextInput
-            value={editedPhone}
-            onChangeText={setEditedPhone}
-          />
-        ) : (
-          <Text>Phone: {customer.phone}</Text>
-        )}
-
-         {/* Pontos e última visita continuam visíveis */}
+        <Text>Name: {customer.name}</Text>
+        <Text>Phone: {customer.phone}</Text>
         <Text>Points: {customer.points}</Text>
         <Text>Last Visit: {customer.lastVisitAt}</Text>
 
-        {/* Botões */}
-        {isEditing ? (
-          <View>
-            <AppButton onPress={handleSave}>
-              <FontAwesome name="save" size={30} color="black" />
-            </AppButton>
-            <AppButton onPress={handleCancelEditing}>
-              <MaterialIcons name="cancel" size={30} color="black" />
-            </AppButton>
-          </View>
-        ) : (
-          <View>
-            <DeleteButton onDelete={handleDelete} size={30} />
-            <AppButton onPress={startEditing} >
-              <FontAwesome name="edit" size={30} color="black" />
-            </AppButton>
-          </View>
-        )}
+        <View>
+          <DeleteButton onDelete={handleDelete} size={30} />
+          <AppButton
+            onPress={() => route.push(`/customers/${customer.id}/edit`)}
+          >
+            <FontAwesome name="edit" size={30} color="black" />
+          </AppButton>
+        </View>
       </View>
-    </GestureHandlerRootView>
+    </>
   );
 }
 
