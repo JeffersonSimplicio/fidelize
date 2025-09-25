5af839932c8df4d283a6859b8c0f4ced0e548e3e
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-09-25 20:42:36 -0300

    feat: implement findById method in CustomerRepositoryDrizzle

--- domain/customers/customer.repository.ts
+++ domain/customers/customer.repository.ts
@@ -4,7 +4,7 @@ import { CreateCustomerDto,  } from './dtos/create-customer.dto';
 
 export interface ICustomerRepository {
   create(data: CreateCustomerDto): Promise<Customer>;
-  // findById(id: number): Promise<Customer | null>;
+  findById(id: number): Promise<Customer | null>;
   // findAll(): Promise<Customer[]>;
   // update(id: number, data: UpdateCustomerDto): Promise<Customer | null>;
   // delete(id: number): Promise<boolean>;
--- repositories/customers.repository.ts
+++ repositories/customers.repository.ts
@@ -4,10 +4,12 @@ import { Customer } from "@/domain/customers/customer.entity";
 import { ICustomerRepository } from "@/domain/customers/customer.repository";
 import { CreateCustomerDto } from "@/domain/customers/dtos/create-customer.dto";
 import { mapDbCustomerToDomain } from "@/repositories/mappers/customerMapper";
+import { eq } from "drizzle-orm";
+
 
 export class CustomerRepositoryDrizzle implements ICustomerRepository {
   constructor(private readonly db: drizzleClient) { }
- 
+
   async create(data: CreateCustomerDto): Promise<Customer> {
     const now = new Date();
 
@@ -23,5 +25,16 @@ export class CustomerRepositoryDrizzle implements ICustomerRepository {
       .returning();
 
     return mapDbCustomerToDomain(inserted);
+  }
+
+  async findById(id: number): Promise<Customer | null> {
+    const [dbCustomer] = await this.db
+      .select()
+      .from(customers)
+      .where(eq(customers.id, id));
+
+    if (!dbCustomer) return null;
+
+    return mapDbCustomerToDomain(dbCustomer);
   }
 }
