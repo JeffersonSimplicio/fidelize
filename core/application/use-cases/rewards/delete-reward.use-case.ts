8e373b001ce95e2186acdae1407398f0b9b4a6c3
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-09-28 15:56:15 -0300

    feat: add use case to delete reward

---
+++ core/application/use-cases/rewards/delete-reward.use-case.ts
@@ -0,0 +1,10 @@
+import { IRewardRepository } from "@/core/domain/rewards/reward.repository";
+import { IDeleteReward } from "@/core/application/interfaces/rewards/delete-reward";
+
+export class DeleteRewardUseCase implements IDeleteReward {
+  constructor(private readonly repo: IRewardRepository) { }
+
+  async execute(id: number): Promise<boolean> {
+    return await this.repo.delete(id);
+  }
+}
