f354b7d77b21a24ffe3cd909446ab07c630f6c83
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-09-28 15:35:37 -0300

    feat: add use case to update reward details

---
+++ core/application/use-cases/rewards/edit-reward-detail.use-case.ts
@@ -0,0 +1,21 @@
+import { Reward } from "@/core/domain/rewards/reward.entity";
+import { IRewardRepository } from "@/core/domain/rewards/reward.repository";
+import { IEditRewardDetail } from "@/core/application/interfaces/rewards/edit-reward-detail";
+import { UpdateRewardDto } from "@/core/application/dtos/rewards/update-reward.dto";
+
+
+export class UpdateRewardUseCase implements IEditRewardDetail {
+  constructor(private readonly repo: IRewardRepository) { }
+
+  async execute(id: number, data: UpdateRewardDto): Promise<Reward | null> {
+    const customer = await this.repo.findById(id);
+
+    if (!customer) return null;
+
+    const updateData: UpdateRewardDto = { ...data };
+
+    const reward = await this.repo.update(id, updateData);
+
+    return reward;
+  }
+}
