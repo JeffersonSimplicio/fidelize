b4bef881396143b9f39bdaad5a42a3a681c85996
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-09-30 13:45:59 -0300

    chore: remove unused custom error file

--- core/domain/customers/errors/empy-phone.error.ts
+++
@@ -1,7 +0,0 @@
-import { BaseError } from "@/core/domain/shared/errors/base.error";
-
-export class EmptyPhoneError extends BaseError {
-  constructor() {
-    super("Telefone não pode ser vazio.");
-  }
-}
