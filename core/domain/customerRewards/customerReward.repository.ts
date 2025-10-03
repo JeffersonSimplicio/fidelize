c5b8d7d63edeeef6dac80d265092351aef934d96
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-10-03 19:54:40 -0300

    feat(customerReward): add repository interface for CustomerRewards

---
+++ core/domain/customerRewards/customerReward.repository.ts
@@ -0,0 +1,9 @@
+import { CustomerReward } from "@/core/domain/customerRewards/customerReward.entity";
+
+export interface ICustomerRewardRepository {
+  create(customer: CustomerReward): Promise<CustomerReward>;
+  findById(id: number): Promise<CustomerReward | null>;
+  findByCustomerId(id: number): Promise<CustomerReward | null>;
+  findByRewardId(id: number): Promise<CustomerReward | null>;
+  delete(id: number): Promise<boolean>;
+}
