8dd11b00e2bf4e21935f71837988504604d4a6d6
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-10-05 09:50:00 -0300

    fix(customerReward): correct return type in repository interface

--- core/domain/customerRewards/customerReward.repository.ts
+++ core/domain/customerRewards/customerReward.repository.ts
@@ -3,7 +3,7 @@ import { CustomerReward } from "@/core/domain/customerRewards/customerReward.ent
 export interface ICustomerRewardRepository {
   create(customer: CustomerReward): Promise<CustomerReward>;
   findById(id: number): Promise<CustomerReward | null>;
-  findByCustomerId(id: number): Promise<CustomerReward | null>;
-  findByRewardId(id: number): Promise<CustomerReward | null>;
+  findByCustomerId(id: number): Promise<CustomerReward[] | null>;
+  findByRewardId(id: number): Promise<CustomerReward[] | null>;
   delete(id: number): Promise<boolean>;
 }
