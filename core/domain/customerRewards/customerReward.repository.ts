4ce439275021ce5ee0b329409f07109a7a427928
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-10-06 15:00:23 -0300

    feat(customer-reward): add find methods to CustomerReward repository

--- core/domain/customerRewards/customerReward.repository.ts
+++ core/domain/customerRewards/customerReward.repository.ts
@@ -1,9 +1,10 @@
 import { CustomerReward } from "@/core/domain/customerRewards/customerReward.entity";
 
 export interface ICustomerRewardRepository {
-  create(customer: CustomerReward): Promise<CustomerReward>;
+  create(customerReward: CustomerReward): Promise<CustomerReward>;
   findById(id: number): Promise<CustomerReward | null>;
-  findByCustomerId(id: number): Promise<CustomerReward[] | null>;
-  findByRewardId(id: number): Promise<CustomerReward[] | null>;
-  delete(id: number): Promise<boolean>;
+  findByCustomerId(id: number): Promise<CustomerReward[]>;
+  findByRewardId(id: number): Promise<CustomerReward[]>;
+  findAll(): Promise<CustomerReward[]>;
+  // delete(id: number): Promise<boolean>;
 }
---
+++ core/infrastructure/repositories/customer-reward.repository.ts
@@ -0,0 +1,52 @@
+import { drizzleClient } from "@/core/infrastructure/database/drizzle/db";
+import { CustomerRewardsTable } from '@/core/infrastructure/database/drizzle/types';
+import { ICustomerRewardRepository } from "@/core/domain/customerRewards/customerReward.repository";
+import { CustomerReward } from "@/core/domain/customerRewards/customerReward.entity";
+import { mapDbCustomerRewardToDomain } from "@/core/infrastructure/mappers/customerRewardMapper";
+import { eq, SQL } from "drizzle-orm";
+
+export class CustomerRewardRepositoryDrizzle implements ICustomerRewardRepository {
+  constructor(
+    private readonly db: drizzleClient,
+    private readonly table: CustomerRewardsTable
+  ) { }
+
+  async create(customerReward: CustomerReward): Promise<CustomerReward> {
+    const [inserted] = await this.db
+      .insert(this.table)
+      .values(customerReward.toPersistence())
+      .returning();
+
+    return mapDbCustomerRewardToDomain(inserted);
+  }
+
+  private async findByCondition(condition: SQL): Promise<CustomerReward[]> {
+    const result = await this.db
+      .select()
+      .from(this.table)
+      .where(condition);
+
+    return result.map(mapDbCustomerRewardToDomain);
+  }
+
+  async findById(id: number): Promise<CustomerReward | null> {
+    const [result] = await this.findByCondition(eq(this.table.id, id));
+    return result || null;
+  }
+
+  async findByCustomerId(id: number): Promise<CustomerReward[]> {
+    return await this.findByCondition(eq(this.table.customerId, id));
+  }
+
+  async findByRewardId(id: number): Promise<CustomerReward[]> {
+    return await this.findByCondition(eq(this.table.rewardId, id));
+  }
+
+  async findAll(): Promise<CustomerReward[]> {
+    const result = await this.db
+      .select()
+      .from(this.table);
+
+    return result.map(mapDbCustomerRewardToDomain);
+  }
+}
