00b7a5700d33dcbcdb4099959389e78523d53a59
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2025-11-21 16:00:11 -0300

    test: add coverage configuration to Jest and update test scripts

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,18 @@ module.exports = {
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
+
+  collectCoverageFrom: [
+    "core/**/*.{ts,tsx}",
+    "!core/**/index.{ts,tsx}",
+    "!core/**/*.d.ts",
+    "!core/**/__tests__/**",
+    "!core/**/migrations/**",
+    "!core/**/types/**",
+    "!core/**/errors/**",
+    "!core/**/*.interface.ts",
+    "!core/**/*.(spec|test).{ts,tsx,js}",
+  ],
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: ["text", "lcov", "cobertura"],
 };
